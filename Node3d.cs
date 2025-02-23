b019c000c953448444ab9051645c096aad379bea
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2025-02-23 02:49:59 -0800

    added optional first person camera, i'll move more code for that to another PR

--- Node3d.cs
+++ Node3d.cs
@@ -5,7 +5,7 @@ public partial class Node3d : Node3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
 			base._Ready();
-			var camera = GetNode<Camera3D>("Camera3D");
+			var firstPersoncamera = GetNode<Camera3D>("Player/Camera3D");
 			//camera.Position.Z = 300;
 	}
 
